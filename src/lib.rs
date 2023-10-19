deaa4df703cbe1dcca0280d8a545ce35f794d6ab
cloudwei <cloudweisz@163.com>
2023-10-19 21:21:06 +0800

    add sell_shares

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 #![no_std]
 
 use gstd::{collections::HashMap, exec, msg, prelude::*, ActorId};
-use kee_bee_io::InitConfig;
+use kee_bee_io::{FBSEvent, InitConfig};
+
+pub mod utils;
 
 static mut KEE_BEE_SHARE: Option<KeeBeeShare> = None;
 const ETH1: u128 = 10 ^ 18;
@@ -12,22 +14,21 @@ pub struct KeeBeeShare {
     pub share_supply: HashMap<ActorId, u128>,
     pub manager: HashMap<ActorId, bool>,
     pub protocol_fee_destination: ActorId,
-    pub protocol_fee_percent:u128,
-    pub subject_fee_percent:u128,
-    pub max_fee_percent:u128,
-    pub max_amount:u8,
+    pub protocol_fee_percent: u128,
+    pub subject_fee_percent: u128,
+    pub max_fee_percent: u128,
+    pub max_amount: u8,
 }
 
 #[no_mangle]
-extern fn init() {
-    let init_config: InitConfig =
-        msg::load().expect("Unable to decode protocoal fee destination");
+extern "C" fn init() {
+    let init_config: InitConfig = msg::load().expect("Unable to decode protocoal fee destination");
     let mut kee_bee_share = KeeBeeShare {
         protocol_fee_destination: init_config.protocol_fee_destination,
-        protocol_fee_percent:init_config.protocol_fee_percent,
-        subject_fee_percent:init_config.subject_fee_percent,
-        max_fee_percent:100000000000000000u128,
-        max_amount:1,
+        protocol_fee_percent: init_config.protocol_fee_percent,
+        subject_fee_percent: init_config.subject_fee_percent,
+        max_fee_percent: 100000000000000000u128,
+        max_amount: 1,
         ..Default::default()
     };
     kee_bee_share.manager.insert(msg::source(), true);
@@ -40,9 +41,8 @@ extern fn init() {
 
 // event Trade(address trader, address subject, bool isBuy, uint256 shareAmount, uint256 ethAmount, uint256 protocolEthAmount, uint256 subjectEthAmount, uint256 supply);
 
-
 impl KeeBeeShare {
-    fn get_price(&self,supply: u128, amount: u128) -> u128 {
+    fn get_price(&self, supply: u128, amount: u128) -> u128 {
         assert!(amount <= self.max_amount.into(), "amount too high");
         let sum1 = if supply == 0 {
             0
@@ -58,38 +58,123 @@ impl KeeBeeShare {
         return summation * ETH1 / 16000u128;
     }
 
-    pub fn get_buy_price(&self,shares_subject:ActorId, amount:u128)->u128 {
-        return self.get_price(*self.share_supply.get(&shares_subject).unwrap_or(&0u128), amount);
+    pub fn get_buy_price(&self, shares_subject: ActorId, amount: u128) -> u128 {
+        return self.get_price(
+            *self.share_supply.get(&shares_subject).unwrap_or(&0u128),
+            amount,
+        );
     }
 
-    pub fn get_sell_price(&self,shares_subject:ActorId, amount:u128) ->u128{
-        return self.get_price(*self.share_supply.get(&shares_subject).unwrap_or(&0u128) - amount, amount);
+    pub fn get_sell_price(&self, shares_subject: ActorId, amount: u128) -> u128 {
+        return self.get_price(
+            *self.share_supply.get(&shares_subject).unwrap_or(&0u128) - amount,
+            amount,
+        );
     }
 
-    pub fn get_buy_price_after_fee(&self,shares_subject:ActorId, amount:u128) ->u128 {
+    pub fn get_buy_price_after_fee(&self, shares_subject: ActorId, amount: u128) -> u128 {
         let price = self.get_buy_price(shares_subject, amount);
-        let protocol_fee = price * self.protocol_fee_percent /  ETH1;
+        let protocol_fee = price * self.protocol_fee_percent / ETH1;
         let subject_fee = price * self.subject_fee_percent / ETH1;
         return price + protocol_fee + subject_fee;
     }
 
-    pub fn buyShares(&mut self,sharesSubject:ActorId, amount:u128){
-        let supply = self.share_supply.get(&sharesSubject).unwrap_or_default();
-        assert!(supply > 0 || sharesSubject == msg::source(), "Only the shares' subject can buy the first share");
-        let price = getPrice(supply, amount);
-        let protocolFee = price * protocolFeePercent / ETH1;
-        let subjectFee = price * subjectFeePercent / ETH1;
-        assert!(msg::value() >= price + protocolFee + subjectFee, "Insufficient payment");
-        let share_balance = **self.shares_balance.get_mut(&sharesSubject).get_or_insert(Default::default()).get_mut(&msg::source()).get_or_insert(&mut 0u128);
-        share_balance = share_balance + amount;
-        // Trade(msg.sender, sharesSubject, true, amount, price, protocolFee, subjectFee, supply + amount);
-        let trade = FBSEvent::Trade{
+    pub fn buy_shares(&mut self, shares_subject: ActorId, amount: u128) {
+        let supply = self.share_supply.get(&shares_subject).unwrap_or(&0).clone();
+        let trader = msg::source();
+        assert!(
+            supply > 0 || shares_subject == trader,
+            "Only the shares' subject can buy the first share"
+        );
+        let price = self.get_price(supply, amount);
+        let protocol_fee = price * self.protocol_fee_percent / ETH1;
+        let subject_fee = price * self.subject_fee_percent / ETH1;
+        assert!(
+            msg::value() >= price + protocol_fee + subject_fee,
+            "Insufficient payment"
+        );
+        self.shares_balance
+            .entry(shares_subject)
+            .or_insert(Default::default())
+            .entry(trader)
+            .and_modify(|share_balance| *share_balance += amount)
+            .or_insert(amount);
+        // sharesSupply[sharesSubject] = supply + amount;
+        self.share_supply
+            .entry(shares_subject)
+            .and_modify(|supply| *supply += amount)
+            .or_insert(amount);
+        msg::send(self.protocol_fee_destination, "", protocol_fee).expect("send ptotocal fee fail");
+        msg::send(shares_subject, "", subject_fee).expect("send subject fee fail");
 
+        // Trade(msg.sender, sharesSubject, true, amount, price, protocolFee, subjectFee, supply + amount);
+        let trade = FBSEvent::Trade {
+            trader,
+            subject: shares_subject,
+            is_buy: true,
+            share_amount: amount,
+            eth_amount: price,
+            protocol_eth_amount: protocol_fee,
+            subject_eth_amount: subject_fee,
+            supply: supply + amount,
         };
-        msg::reply(trade, 0);
-        msg::send(self.protocol_fee_destination,"",protocolFee).expect("send ptotocal fee fail");
-        // (bool success1, ) = protocolFeeDestination.call{value: protocolFee}("");
-        // (bool success2, ) = sharesSubject.call{value: subjectFee}("");
-        msg::send(sharesSubject,"",subjectFee).expect("send subject fee fail");
+        msg::reply(trade, 0).unwrap();
     }
+
+    pub fn sell_shares(&mut self, shares_subject: ActorId, amount: u128) {
+        let supply = self.share_supply.get(&shares_subject).unwrap_or(&0).clone();
+        let trader = msg::source();
+        assert!(supply > amount, "Cannot sell the last share");
+        let price = self.get_price(supply - amount, amount);
+        let protocol_fee = price * self.protocol_fee_percent / ETH1;
+        let subject_fee = price * self.subject_fee_percent / ETH1;
+        let share_balance = self
+            .shares_balance
+            .get(&shares_subject)
+            .unwrap()
+            .get(&trader)
+            .unwrap()
+            .clone();
+        assert!(share_balance >= amount, "Insufficient shares");
+        self.shares_balance
+            .get_mut(&shares_subject)
+            .unwrap()
+            .entry(trader)
+            .and_modify(|share| *share -= amount);
+        self.share_supply
+            .entry(shares_subject)
+            .and_modify(|supply| *supply -= amount);
+
+        msg::send(trader, "", price - protocol_fee - subject_fee).unwrap();
+        msg::send(self.protocol_fee_destination, "", protocol_fee).unwrap();
+        msg::send(shares_subject, "", subject_fee).expect("send subject fee fail");
+        let trade = FBSEvent::Trade {
+            trader,
+            subject: shares_subject,
+            is_buy: false,
+            share_amount: amount,
+            eth_amount: price,
+            protocol_eth_amount: protocol_fee,
+            subject_eth_amount: subject_fee,
+            supply: supply + amount,
+        };
+        msg::reply(trade, 0).unwrap();
+    }
+
+    pub fn set_max_amount(&mut self,max_amount:u8){
+        assert!(self.manager.get(&msg::source()).expect("not manager"),"not manager");
+        self.max_amount = max_amount;
+    }
+
+    // function setFeeDestination(address _feeDestination) public onlyManager {
+    //     protocolFeeDestination = _feeDestination;
+    // }
+
+    // function setProtocolFeePercent(uint256 _feePercent) public onlyManager {
+    //     protocolFeePercent = _feePercent;
+    // }
+
+    // function setSubjectFeePercent(uint256 _feePercent) public onlyManager {
+    //     subjectFeePercent = _feePercent;
+    // }
 }
